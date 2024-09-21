9d2ea09669e69e64b09ba01d968908ee31b3035e
joaoveasey <jovepedro@gmail.com>
2024-09-21 11:54:11 -0300

    feat: adicionando pastas do modelos de domínio (cao, localização, porprietario e usuário)

---
+++ ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Cao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoCaninaApp.Domain.Models
+{
+    public class Cao
+    {
+        public int CaoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Idade { get; set; }
+        public string Raca { get; set; }
+        public int Genero { get; set; } // 1-M | 2-F
+        public int ProprietarioId { get; set; }
+    }
+}
---
+++ ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Proprietario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoCaninaApp.Domain.Models
+{
+    public class Proprietario
+    {
+        public int ProprietarioId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
