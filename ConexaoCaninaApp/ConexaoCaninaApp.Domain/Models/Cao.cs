ccff8bcecf5a6b307dded3fbfa0ca74f157ecfe2
joaoveasey <jovepedro@gmail.com>
2024-09-21 13:26:45 -0300

    feat: construção do domínio com 4 entidades e validações básicas concluída

--- ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Cao.cs
+++ ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Cao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,27 @@ namespace ConexaoCaninaApp.Domain.Models
     public class Cao
     {
         public int CaoId { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O nome do cão é obrigatório.")]
+        [StringLength(30,ErrorMessage = "O nome do cão deve ter no máximo 30 caracteres.")]
+        public string? Nome { get; set; }
+
+        [StringLength(500, ErrorMessage = "A descrição do cão deve ter no máximo 500 caracteres.")]
+        public string? Descricao { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "A idade do cão deve ser maior que 0.")]
         public int Idade { get; set; }
-        public string Raca { get; set; }
+
+        [Required(ErrorMessage = "A raça do cão é obrigatória.")]
+        public string? Raca { get; set; }
+
+        [Required(ErrorMessage = "O gênero do cão é obrigatório.")]
         public int Genero { get; set; } // 1-M | 2-F
+
+        // chave estrangeira
         public int ProprietarioId { get; set; }
+
+        // propriedade de navegação
+        public Proprietario? Proprietario { get; set; }
     }
 }
--- ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Proprietario.cs
+++ ConexaoCaninaApp/ConexaoCaninaApp.Domain/Models/Proprietario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,25 @@ namespace ConexaoCaninaApp.Domain.Models
     public class Proprietario
     {
         public int ProprietarioId { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
-        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O nome do proprietário é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do proprietário deve ter no máximo 100 caracteres.")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail do proprietário é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone do proprietário é obrigatório.")]
+        [Phone(ErrorMessage = "O telefone informado não é valido.")]
+        public string? Telefone { get; set; }
+
+
+        // chave estrangeira
+        public int LocalizacaoId { get; set; }
+
+        // propriedades de navegação
+        public Localizacao Localizacao { get; set; }
+        public ICollection<Cao>? Caes { get; set; } // um proprietário pode ter vários cães
     }
 }
