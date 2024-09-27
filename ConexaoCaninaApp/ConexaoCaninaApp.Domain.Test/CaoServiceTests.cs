4064ef94ea272149988dc911e1747cabb0a882a3
joaoveasey <jovepedro@gmail.com>
2024-09-27 12:21:39 -0300

    feat: correções de build feitas. foi necessário alterar o pipeline da azure

--- ConexaoCaninaApp/ConexaoCaninaApp.Domain.Test/CaoServiceTests.cs
+++ ConexaoCaninaApp/ConexaoCaninaApp.Domain.Test/CaoServiceTests.cs
@@ -3,7 +3,7 @@ using ConexaoCaninaApp.Application.Interfaces;
 using ConexaoCaninaApp.Application.Services;
 using ConexaoCaninaApp.Domain.Models;
 using ConexaoCaninaApp.Infra.Data.Interfaces;
-//using Moq;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,125 +15,125 @@ namespace ConexaoCaninaApp.Domain.Test
 {
 	public class CaoServiceTests
 	{
-		//private readonly CaoService _caoService;
-		//private readonly Mock<ICaoRepository> _mockCaoRepository;
-		//private readonly Mock<INotificacaoService> _mockNotificacaoService;
+		private readonly CaoService _caoService;
+		private readonly Mock<ICaoRepository> _mockCaoRepository;
+		private readonly Mock<INotificacaoService> _mockNotificacaoService;
 
-		//public CaoServiceTests()
-		//{
-		//	_mockCaoRepository = new Mock<ICaoRepository>();
-		//	_mockNotificacaoService = new Mock<INotificacaoService>();
-		//	_caoService = new CaoService(_mockCaoRepository.Object, _mockNotificacaoService.Object);
-		//}
+		public CaoServiceTests()
+		{
+			_mockCaoRepository = new Mock<ICaoRepository>();
+			_mockNotificacaoService = new Mock<INotificacaoService>();
+			_caoService = new CaoService(_mockCaoRepository.Object, _mockNotificacaoService.Object);
+		}
 
-		//[Fact]
-		//public async Task ObterPorId_Deve_Retornar_Cao_Quando_Existe()
-		//{
-		//	// ARRANGE
+		[Fact]
+		public async Task ObterPorId_Deve_Retornar_Cao_Quando_Existe()
+		{
+			// ARRANGE
 
-		//	var caoId = 1;
-		//	var expectedCao = new Cao
-		//	{
-		//		CaoId = caoId,
-		//		Nome = "Deus Imperador PHG"
-		//	};
+			var caoId = 1;
+			var expectedCao = new Cao
+			{
+				CaoId = caoId,
+				Nome = "Deus Imperador PHG"
+			};
 
-		//	_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
-		//	.ReturnsAsync(expectedCao);
+			_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
+			.ReturnsAsync(expectedCao);
 
-		//	// ACT
+			// ACT
 
-		//	var result = await _caoService.ObterPorId(caoId);
+			var result = await _caoService.ObterPorId(caoId);
 
-		//	// ASSERT
+			// ASSERT
 
-		//	Assert.NotNull(result);
-		//	Assert.Equal(caoId, result.CaoId);
-		//	Assert.Equal("Deus Imperador PHG", result.Nome);
-		//}
+			Assert.NotNull(result);
+			Assert.Equal(caoId, result.CaoId);
+			Assert.Equal("Deus Imperador PHG", result.Nome);
+		}
 
-		//[Fact]
-		//public async Task ObterPorId_Deve_Retornar_Null_Quando_Cao_Nao_Existe()
-		//{
-		//	// ARRANGE
+		[Fact]
+		public async Task ObterPorId_Deve_Retornar_Null_Quando_Cao_Nao_Existe()
+		{
+			// ARRANGE
 
-		//	var caoId = 1;
+			var caoId = 1;
 
-		//	_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
-		//   .ReturnsAsync((Cao)null);
+			_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
+		   .ReturnsAsync((Cao)null);
 
-		//	// ACT
+			// ACT
 
-		//	var result = await _caoService.ObterPorId(caoId);
+			var result = await _caoService.ObterPorId(caoId);
 
-		//	// ASSERT
+			// ASSERT
 
-		//	Assert.Null(result);
-		//}
+			Assert.Null(result);
+		}
 
-		//[Fact]
-		//public async Task AdicionarCao_Deve_Chamar_Repositorio_E_Notificar_Administrador()
-		//{
-		//	// ARRANGE
+		[Fact]
+		public async Task AdicionarCao_Deve_Chamar_Repositorio_E_Notificar_Administrador()
+		{
+			// ARRANGE
 
-		//	var caoDto = new CaoDto
-		//	{
-		//		Nome = "Deus Imperador PHG",
-		//		Raca = "Pastor Alemão",
-		//		Idade = 20,
-		//		Tamanho = TamanhoCao.Grande,
-		//		ProprietarioId = 1
-		//	};
+			var caoDto = new CaoDto
+			{
+				Nome = "Deus Imperador PHG",
+				Raca = "Pastor Alemão",
+				Idade = 20,
+				Tamanho = TamanhoCao.Grande,
+				ProprietarioId = 1
+			};
 
-		//	var novoCao = new Cao
-		//	{
-		//		Nome = caoDto.Nome,
-		//		Raca = caoDto.Raca,
-		//		Idade = caoDto.Idade,
-		//		Genero = caoDto.Genero,
-		//		Tamanho = caoDto.Tamanho,
-		//		ProprietarioId = caoDto.ProprietarioId,
-		//		Status = StatusCao.Pendente
-		//	};
+			var novoCao = new Cao
+			{
+				Nome = caoDto.Nome,
+				Raca = caoDto.Raca,
+				Idade = caoDto.Idade,
+				Genero = caoDto.Genero,
+				Tamanho = caoDto.Tamanho,
+				ProprietarioId = caoDto.ProprietarioId,
+				Status = StatusCao.Pendente
+			};
 
-		//	// ACT
+			// ACT
 
-		//	var result = await _caoService.AdicionarCao(caoDto);
+			var result = await _caoService.AdicionarCao(caoDto);
 
-		//	// ASSERT
+			// ASSERT
 
-		//	_mockCaoRepository.Verify(repo => repo.Adicionar(It.IsAny<Cao>()), Times.Once);
-		//	_mockNotificacaoService.Verify(notif => notif.EnviarNotificacaoParaAdministrador(It.IsAny<Cao>()), Times.Once);
-		//	Assert.NotNull(result);
-		//	Assert.Equal(StatusCao.Pendente, result.Status);
-		//}
+			_mockCaoRepository.Verify(repo => repo.Adicionar(It.IsAny<Cao>()), Times.Once);
+			_mockNotificacaoService.Verify(notif => notif.EnviarNotificacaoParaAdministrador(It.IsAny<Cao>()), Times.Once);
+			Assert.NotNull(result);
+			Assert.Equal(StatusCao.Pendente, result.Status);
+		}
 
-		//[Fact]
-		//public async Task AprovarCao_Deve_Atualizar_Status_Para_Aprovado_E_Notificar_Usuario()
-		//{
-		//	// ARRANGE
+		[Fact]
+		public async Task AprovarCao_Deve_Atualizar_Status_Para_Aprovado_E_Notificar_Usuario()
+		{
+			// ARRANGE
 
-		//	var caoId = 1;
-		//	var caoExistente = new Cao
-		//	{
-		//		CaoId = caoId,
-		//		Nome = "Deus Imperador PHG",
-		//		Status = StatusCao.Pendente
-		//	};
+			var caoId = 1;
+			var caoExistente = new Cao
+			{
+				CaoId = caoId,
+				Nome = "Deus Imperador PHG",
+				Status = StatusCao.Pendente
+			};
 
-		//	_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
-		//   .ReturnsAsync(caoExistente);
+			_mockCaoRepository.Setup(repo => repo.ObterPorId(caoId))
+		   .ReturnsAsync(caoExistente);
 
-		//	// ACT
+			// ACT
 
-		//	await _caoService.AprovarCao(caoId);
+			await _caoService.AprovarCao(caoId);
 
-		//	// ASSERT
+			// ASSERT
 
-		//	_mockCaoRepository.Verify(repo => repo.Atualizar(It.IsAny<Cao>()), Times.Once);
-		//	_mockNotificacaoService.Verify(notif => notif.EnviarNotificacaoParaUsuario(It.IsAny<Cao>()), Times.Once);
-		//	Assert.Equal(StatusCao.Aprovado, caoExistente.Status);
-		//}
+			_mockCaoRepository.Verify(repo => repo.Atualizar(It.IsAny<Cao>()), Times.Once);
+			_mockNotificacaoService.Verify(notif => notif.EnviarNotificacaoParaUsuario(It.IsAny<Cao>()), Times.Once);
+			Assert.Equal(StatusCao.Aprovado, caoExistente.Status);
+		}
 	}
 }
 
